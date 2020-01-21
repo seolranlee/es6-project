fec989c78a11485ebb7796b5aa47e7afe4535945
seolran <leesugar90@gmail.com>
2020-01-21 18:39:24 +0900

    [Babel과 Webpack을 이용한 ES6 환경 구축]
    <Babel>
    - Babel CLI 설치
    - .babelrc 설정 파일 작성
    - 트랜스파일링
    - Babel 플러그인(plugin-proposal-class-properties)
    - 브라우저에서 모듈 로딩 테스트
    
    <Webpack>
    - Webpack 설치
    - babel-loader
    - webpack.config.js
    - babel-polyfill(구형 브라우저가 지원하지 않는 코드가 남아있을 수 있는 것을 대비(Promise, Object.assign, Array.from 등))
    - Sass 컴파일
    + 컴파일된 css를 bundle.js 파일에 포함시키는 방법
    + 컴파일된 CSS를 별도의 CSS 파일로 분리하는 방법

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    // enntry file
+    // entry files
     entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
     // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
+    plugins: [
+        // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+        new MiniCssExtractPlugin({ filename: 'css/style.css' })
+    ],
     module: {
         rules: [
             {
@@ -15,21 +20,21 @@ module.exports = {
                 include: [
                     path.resolve(__dirname, 'src/js')
                 ],
-                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env'],
                         plugins: ['@babel/plugin-proposal-class-properties']
                     }
-                }
+                },
+                exclude: /node_modules/
             },
             {
                 test: /\.scss$/,
                 use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader",   // translates CSS into CommonJS
-                    "sass-loader"   // compiles Sass to CSS, using Node Sass by default
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
                 ],
                 exclude: /node_modules/
             }
